c84f9f1fdc63fa69116f2dfdd9cb1e62847dd6be
pataya235 <felixa96@web.de>
2022-03-29 19:27:54 +0200

    vectors

---
+++ 04_Vector/Vector1st.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> my_vector(2, 0);
+
+    for (std::size_t i = 0; i < my_vector.size(); i++)
+    {
+        std::cout << my_vector[i] << std::endl;
+    }
+    std::cout << std::endl;
+    my_vector[0] = 11;
+    my_vector[1] = -42;
+
+    for (std::size_t i = 0; i < my_vector.size(); i++)
+    {
+        std::cout << my_vector[i] << std::endl;
+    }
+    std::cout << std::endl;
+    my_vector.push_back(55);
+
+    for (std::size_t i = 0; i < my_vector.size(); i++)
+    {
+        std::cout << my_vector[i] << std::endl;
+    }
+    std::cout << std::endl;
+    my_vector.pop_back();
+
+    for (std::size_t i = 0; i < my_vector.size(); i++)
+    {
+        std::cout << my_vector[i] << std::endl;
+    }
+
+    return 0;
+}
---
+++ 04_Vector/VectorSizeCapacity.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> my_vector;
+    for (int i = 0; i < 9; i++)
+    {
+        my_vector.push_back(i);
+    }
+
+    std::cout << "Size: " << my_vector.size() << std::endl;
+    std::cout << "Capacity: " << my_vector.capacity() << std::endl;
+
+    return 0;
+}
