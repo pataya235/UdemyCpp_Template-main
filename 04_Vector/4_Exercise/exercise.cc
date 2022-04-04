310bc782704f84aad0e577be464ed5ee47dd76cc
pataya235 <felixa96@web.de>
2022-04-04 21:42:25 +0200

    vec_exercise

--- 04_Vector/4_Exercise/exercise.cc
+++ 04_Vector/4_Exercise/exercise.cc
@@ -7,9 +7,26 @@
 // Exercise 2
 std::vector<double> max_row_values(Matrix &matrix)
 {
+    std::vector<double> ret_vec(matrix.size(), 0.0);
+    for (int i = 0; i != matrix.size(); i++)
+    {
+        double act_row_max = 0.0;
+        for (int j = 0; j != matrix[i].size(); j++)
+        {
+            if (matrix[i][j] > act_row_max)
+            {
+                act_row_max = matrix[i][j];
+            }
+        }
+        ret_vec[i] = act_row_max;
+    }
+    return ret_vec;
 }
 
 // Exercise 3
+
 double sort_and_max(std::vector<double> &vec)
 {
+    std::sort(vec.begin(), vec.end());
+    return vec.back();
 }
