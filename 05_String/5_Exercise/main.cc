654fcadf17308b965a23a3a30c300b8f7141cbbd
pataya235 <felixa96@web.de>
2022-04-15 21:08:34 +0200

    encrypt exercise

--- 05_String/5_Exercise/main.cc
+++ 05_String/5_Exercise/main.cc
@@ -8,15 +8,15 @@
 int main()
 {
     std::ifstream iffile;
-    iffile.open("text.txt", std::ios::in);
+    iffile.open("text.txt", std::ios::in); //read text in "in" mode
 
     std::string content;
-    iffile >> content;
+    iffile >> content; //copy text from iffile to content
     iffile.close();
 
-    ByteArray plain_text(content.begin(), content.end());
-    ByteArray key(8, 0xff);
-    ByteArray cipher_text = hex_vector_xor(plain_text, key);
+    ByteArray plain_text(content.begin(), content.end());    //iterate through string and save in byte/char array
+    ByteArray key(8, 0xff);                                  //8 bit key in byte array
+    ByteArray cipher_text = hex_vector_xor(plain_text, key); // every char XOR key
 
     for (std::size_t i = 0; i < cipher_text.size(); ++i)
     {
@@ -24,7 +24,7 @@ int main()
     }
     std::cout << std::endl;
 
-    ByteArray message = hex_vector_xor(cipher_text, key);
+    ByteArray message = hex_vector_xor(cipher_text, key); //back XOR to decrypt
 
     for (std::size_t i = 0; i < message.size(); ++i)
     {
