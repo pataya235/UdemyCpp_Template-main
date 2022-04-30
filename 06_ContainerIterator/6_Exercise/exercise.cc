ca99d9d28b429383cc9588b755ab5522b32e738c
pataya235 <felixa96@web.de>
2022-04-30 14:40:34 +0200

    ContainerIterator_exercise

--- 06_ContainerIterator/6_Exercise/exercise.cc
+++ 06_ContainerIterator/6_Exercise/exercise.cc
@@ -1,12 +1,37 @@
-#include <iostream>
-
 #include "exercise.h"
+#include <iostream>
 
 // Exercise 2
 std::string get_oldest_friend(const Friends &friends)
 {
+    std::string oldest_friend = "";
+    int oldest_age = 0;
+
+    for (const auto &val : friends)
+    {
+        std::pair<int, int> p = val.second; //val.second affects second map entry == pair
+        if (p.first > oldest_age)           //p.first affects first pair enty == age
+        {
+            oldest_age = p.first;      //age
+            oldest_friend = val.first; //name
+        }
+    }
+    return oldest_friend;
 }
 
 std::string get_heaviest_friend(const Friends &friends)
 {
+    std::string heaviest_friend = "";
+    int heaviest_weight = 0;
+
+    for (const auto &val : friends)
+    {
+        std::pair<int, int> p = val.second; //val.second affects second map entry == pair
+        if (p.second > heaviest_weight)     //p.first affects first pair enty == age
+        {
+            heaviest_weight = p.second;  //weight
+            heaviest_friend = val.first; //name
+        }
+    }
+    return heaviest_friend;
 }
--- 06_ContainerIterator/6_Exercise/exercise.h
+++ 06_ContainerIterator/6_Exercise/exercise.h
@@ -5,7 +5,7 @@
 #include <utility>
 
 // Exercise 1
-// ...
+typedef std::map<std::string, std::pair<int, int>> Friends; //typedef useful for long statements
 
 // Exercise  2
 std::string get_oldest_friend(const Friends &friends);
