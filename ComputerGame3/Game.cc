272359fe095074c3592ae793fb533663bb507870
pataya235 <felixa96@web.de>
2022-05-03 21:24:18 +0200

    game with 2d field

---
+++ ComputerGame3/Game.cc
@@ -0,0 +1,139 @@
+#include "Game.h"
+#include <iostream>
+
+ConsoleInput map_user_input(char user_input)
+{
+    switch (user_input)
+    {
+    case 'a':
+    {
+        return ConsoleInput::LEFT;
+    }
+    case 'd':
+    {
+        return ConsoleInput::RIGHT;
+    }
+    case 'w':
+    {
+        return ConsoleInput::UP;
+    }
+    case 's':
+    {
+        return ConsoleInput::DOWN;
+    }
+    default:
+        return ConsoleInput::INVALID;
+    }
+}
+
+void print_game_state(Position player)
+{
+    GameState game_state(LEN_X, std::string(LEN_Y, '.')); //Y Spalten, X Zeilen
+
+    game_state[START.first][START.second] = '|';
+    game_state[GOAL.first][GOAL.second] = '|';
+    game_state[player.first][player.second] = 'P';
+    for (unsigned int i = 0; i < LEN_X; i++) //Konsolen-Ausgabe
+    {
+        for (unsigned int j = 0; j < LEN_Y; j++)
+        {
+            std::cout << game_state[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+Position execute_move(Position player, ConsoleInput move)
+{
+    switch (move)
+    {
+    case ConsoleInput::LEFT:
+    {
+        if (player.second > START.second)
+        {
+            player.second--;
+            std::cout << "You moved to the left" << std::endl;
+        }
+        else
+        {
+            std::cout << "You bounced" << std::endl;
+        }
+        break;
+    }
+    case ConsoleInput::RIGHT:
+    {
+        if (player.second < GOAL.second)
+        {
+            player.second++;
+            std::cout << "You moved to the right" << std::endl;
+        }
+        else
+        {
+            std::cout << "You bounced" << std::endl;
+        }
+        break;
+    }
+    case ConsoleInput::UP:
+    {
+        if (player.first > START.first)
+        {
+            player.first--;
+            std::cout << "You moved upwards" << std::endl;
+        }
+        else
+        {
+            std::cout << "You bounced" << std::endl;
+        }
+        break;
+    }
+    case ConsoleInput::DOWN:
+    {
+        if (player.first < GOAL.first)
+        {
+            player.first++;
+            std::cout << "You moved downwards" << std::endl;
+        }
+        else
+        {
+            std::cout << "You bounced" << std::endl;
+        }
+        break;
+    }
+    case ConsoleInput::INVALID:
+    default:
+    {
+        std::cout << "Unrecognized move" << std::endl;
+        break;
+    }
+    }
+    return player;
+}
+
+bool is_finished(Position player)
+{
+    bool finished = false;
+    if (GOAL == player) //operator ist überladen für pair, jeweils beide Pair-Werte werden verglichen
+    {
+        finished = true;
+        std::cout << "You won" << std::endl;
+    }
+    return finished;
+}
+
+void game()
+{
+    Position player = {0, 0};
+    char user_input;
+    ConsoleInput move;
+    bool finished = false;
+
+    while (!finished)
+    {
+        print_game_state(player);
+        std::cin >> user_input;
+        move = map_user_input(user_input);
+        system("clear");
+        player = execute_move(player, move);
+        finished = is_finished(player);
+    }
+}
---
+++ ComputerGame3/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <utility>
+#include <vector>
+
+using Position = std::pair<unsigned int, unsigned int>; //typedef
+using GameState = std::vector<std::string>;             //Spalten<Zeilen>
+
+constexpr unsigned int LEN_X = 5;
+constexpr unsigned int LEN_Y = 5;
+constexpr Position START = {0, 0};
+constexpr Position GOAL = {LEN_X - 1, LEN_Y - 1};
+
+enum class ConsoleInput : int
+{
+    LEFT = 0,
+    RIGHT = 1,
+    UP = 2,
+    DOWN = 3,
+    INVALID = 4
+};
+
+ConsoleInput map_user_input(char user_input);
+void game();
+void print_game_state(Position player);
+Position execute_move(Position player, ConsoleInput move);
+bool is_finished(Position player);
